03a9d87fda2089c1c76c4f01dcf4dbd3105298bc
SSD Matt <matthecrazyguy@gmail.com>
2023-06-01 15:23:04 -0400

    6/1/23 -- we're getting closer

--- chord.py
+++ chord.py
@@ -4,72 +4,100 @@
 
 from manim import *
 from numpy import ndarray, array
-from anim import *
-
-"""
-cache is purely for optimizing self.[blank] calls in animation/updater functions
-that are called basically every frame
-"""
-from functools import cache
 
 
 class ChordCircle(VMobject):
     def __init__(
         self,
-        dot_radius: float = 0.1,
-        dot_opacity: float = 0.0,
-        dot_color: str = None,
+        center_x: float = 0.0,
+        center_y: float = 0.0,
+        center_dot_color: str = WHITE,
+        dot_radii: float = 0.1,
+        trav_dot_opacity: float = 0.0,
+        trav_dot_color: str = None,
         circle_radius: float = 3.0,
         circle_color: str = RED,
-        center: ndarray = array([-3, 0, 0]),
-        center_color: str = WHITE,
         mov_color: str = BLUE,  # mov_line color
         stat_color: str = PURPLE,  # stat_line color
         chord_color: str = ORANGE,  # chord_line color
         angle_radius: float = 0.5,
         angle_color: str = YELLOW,
-        init_prop: float = 0,  # init point from proportion of circle
+        init_degrees: float = 0.0,
     ):
         # redraw functions, called every frame
+
+        def get_center_dot():
+            return Dot(
+                point=np.array(
+                    [self.center_x.get_value(), self.center_y.get_value(), 0.0]
+                ),
+                radius=dot_radii,
+                color=center_dot_color,
+            )
+
+        def get_circle():
+            return Circle(
+                radius=circle_radius,
+                color=circle_color,
+                arc_center=self.center_dot.get_center(),
+            )
+
+        def get_stat_line():
+            return Line(
+                start=self.center_dot.get_center(),
+                end=self.circle.point_from_proportion(0),
+                color=stat_color,
+            )
+
         def get_trav_dot():
             wiggle = 0
-            circ_prop = (self.theta / 360) % 1
+            circ_prop = (self.theta.get_value() / 360) % 1
             if circ_prop == 1 or circ_prop == 0:
                 """
-                tiny initial position offset, for same reason as explained in
-                go_around_circle(). Otherwise, the first frame can't render
+                There has to be a tiny wiggle/offset to trav_dot's position
+                whenever it gets to the start of the circle to prevent mov_line
+                and stat_line from ever being parallel so that the construction
+                of the Angle object in get_angle() doesn't crap itself.
                 """
-                wiggle = self.center_offset
+                wiggle = UP * 1  # lil wiggle
             return Dot(
                 point=self.circle.point_from_proportion(circ_prop) + wiggle,
-                radius=dot_radius,
-                color=dot_color,
-                fill_opacity=dot_opacity,
+                radius=dot_radii,
+                color=trav_dot_color,
+                fill_opacity=trav_dot_opacity,
             )
 
         def get_mov_line():
-            return Line(center, self.trav_dot.get_center(), color=mov_color)
+            return Line(
+                start=self.center_dot.get_center(),
+                end=self.trav_dot.get_center(),
+                color=mov_color,
+            )
 
         def get_chord_line():
-            return Line(self.trav_dot.get_center(), ORIGIN, color=chord_color)
+            return Line(
+                start=self.trav_dot.get_center(),
+                end=self.stat_line.get_end(),
+                color=chord_color,
+            )
 
         def get_angle():
             return Angle(
-                self.stat_line, self.mov_line, angle_radius, color=angle_color
+                line1=self.stat_line,
+                line2=self.mov_line,
+                radius=angle_radius,
+                color=angle_color,
             )
 
-        # static mobjects (no move)
-        self.theta = ValueTracker(init_prop * 360)
-        self.center_dot = Dot(
-            point=center, radius=dot_radius, color=center_color
-        )
-        self.circle = Circle(
-            radius=circle_radius, color=circle_color, arc_center=center
-        )
-        self.stat_line = Line(center, ORIGIN, color=stat_color)
-        self.center_offset = UP * 0.001
+        # ValueTrackerz
+        self.center_x = ValueTracker(center_x)
+        self.center_y = ValueTracker(center_y)
+        self.theta = ValueTracker(init_degrees)
 
         # dynamic mobjects (do move)
+        self.center_dot = always_redraw(get_center_dot)
+        self.circle = always_redraw(get_circle)
+        self.stat_line = always_redraw(get_stat_line)
         self.trav_dot = always_redraw(get_trav_dot)
         self.mov_line = always_redraw(get_mov_line)
         self.chord_line = always_redraw(get_chord_line)
@@ -87,46 +115,27 @@ class ChordCircle(VMobject):
         self.add(self.chord_line)
         self.add(self.center_dot)
 
-    @cache
-    def spin_chord(self, speed: float, rotations: float):
-        # updater function
-        def go_around_circle(mob, dt):
-            """
-            dt = difference (in seconds) since last frame. At 60fps render, this
-            is equivalent to 1/60. Even though this parameter is not at all
-            necessary to achieve what we want go_around_circle's to do (you can
-            replace it with 1/config.frame_rate, as is done in anim.py), it is
-            necessary to pass in because otherwise the3 updater will literally
-            not fucking do anything
-            """
-            self.theta = (self.theta + (dt * speed)) % 1
-
-            """
-            There has to be a tiny offset to trav_dot's position whenever it
-            gets to the start of the circle to prevent mov_line and stat_line
-            from ever being parallel so that the construction of the Angle
-            object in get_angle() doesn't crap itself
-            """
-            wiggle = 0
-            if self.theta == 1 or self.theta == 0:
-                wiggle = self.center_offset
-
-            mob.move_to(self.circle.point_from_proportion(self.theta) + wiggle)
-
-        # attach updater and wait
-        self.trav_dot.add_updater(go_around_circle)
-        self.wait(rotations / speed)
-
-        # can updater after sum secs and slow to full stop
-        self.trav_dot.remove_updater(go_around_circle)
-        # self.play(quarter_slo_down(self, speed, runtime=1))
+    def set_theta(self, theta):
+        return self.theta.animate.set_value(theta)
 
 
-# TODO: run!! if works, delete all bloat
+"""
+1) make cc.theta.animate.set_value(720) be a return value of a ChordCircle
+   function, ensuring proper OOP
+2) make the center of the ChordCircle be a ValueTracker that all submobjects
+   orient themelves around; when you want to move the ChordCircle, you just put
+   in self.play a call to a ChordCircle function that returns something like
+   self.center_point.animate.set_value(point_to_move_to)
+"""
+
+
 class DemonstrateChord(Scene):
     def construct(self):
-        cc = ChordCircle()
+        cc = ChordCircle(center_x=0, trav_dot_opacity=1.0, trav_dot_color=GREEN)
         self.add(cc)
 
-        theta = cc.theta
-        self.play(theta.animate.set_value(1080), run_time=12)
+        self.play(cc.set_theta(720), run_time=6)
+        self.wait()
+        self.play(cc.center_y.animate.set_value(1), run_time=1)
+        # self.play(FadeOut(cc.chord_line))
+        self.play(cc.set_theta(810), run_time=1)
